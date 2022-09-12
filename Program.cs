24ebfe61b63269d59992bccd923970f578a575ef
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-12 11:58:04 -0500

    Practising Variables

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+// Practicing Variables
+
+string myName = "Andre Renaud";
+int myAge = 13;
+string myUsername = "AndreTheCoder";
+string myPassword = "password";
+bool isAuthenticated = false;
+char myMiddleInitial = 'S';
+double myCreditCardNumber = 1234_1234_1234_1234;
+decimal myCreditCardPin = 1234;
+
+Console.WriteLine($"Name: {myName}");
+Console.WriteLine($"Age: {myAge}");
+Console.WriteLine($"Middle initial: {myMiddleInitial}");
+Console.WriteLine($"Credit card number: {myCreditCardNumber}");
+Console.WriteLine($"Credit card pin: {myCreditCardPin}");
+Console.WriteLine($"Username: {myUsername}");
+Console.WriteLine($"Password: {myPassword}");
+Console.WriteLine($"Purchase authentication: {isAuthenticated}");
+Console.WriteLine($" ");
+Console.WriteLine($"Account Balance too low");
